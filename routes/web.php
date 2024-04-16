35129db72d92748b56d4747c60df1bd38e98057b
Eka Krisna Ferian <kresnaeka.07@gmail.com>
2024-04-16 19:24:50 +0700

    Tugas JS2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\BabyKidController;
+use App\Http\Controllers\BeautyHealthController;
+use App\Http\Controllers\FoodBeverageController;
+use App\Http\Controllers\HomeCareController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\UserController;
+
+/**
+ * Route for Home view
+ */
+Route::get('/', [HomeController::class, 'index']);
+
+
+/**
+ * Route prefix Category
+ */
+Route::prefix('category')->group(function(){
+    Route::get('/food-beverage', [FoodBeverageController::class, 'index']);
+    Route::get('/beauty-health', [BeautyHealthController::class, 'index']);
+    Route::get('/home-care', [HomeCareController::class, 'index']);
+    Route::get('/baby-kid', [BabyKidController::class, 'index']);
 });
+
+/**
+ * Route Params User View
+ */
+Route::get('/user/{id}/name/{name}', [UserController::class, 'index']);
+
+/**
+ * Route Penjualan
+ */
+Route::get('/penjualan', [PenjualanController::class, 'index']);
